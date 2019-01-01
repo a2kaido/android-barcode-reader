589b3d4459c9af61141c895d87104907d6c20d31
a2kaido <amane.nikaido+a2kaido@gmail.com>
2019-01-01 17:41:55 +0900

    init

---
+++ app/src/main/java/io/github/a2kaido/barcode/reader/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.a2kaido.barcode.reader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
