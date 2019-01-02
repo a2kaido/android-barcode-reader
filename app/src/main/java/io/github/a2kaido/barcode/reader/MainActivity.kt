bf2681c45eaf93e53a66babb26d0837b88b1ccc6
a2kaido <amane.nikaido+a2kaido@gmail.com>
2019-01-02 12:39:12 +0900

    add bottom nav.

--- app/src/main/java/io/github/a2kaido/barcode/reader/MainActivity.kt
+++ app/src/main/java/io/github/a2kaido/barcode/reader/MainActivity.kt
@@ -1,12 +1,42 @@
 package io.github.a2kaido.barcode.reader
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.main_fragment)
+        bottom_nav.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, arguments ->
+            println(destination)
+            println(arguments)
+        }
+
+        bottom_nav.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_scan -> {
+                    findNavController(R.id.main_fragment).navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.menu_history -> {
+                    findNavController(R.id.main_fragment).navigate(R.id.historyFragment)
+                    true
+                }
+                R.id.menu_settings -> {
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
     }
 }
