76a3347db492b830e5fac379246888e5662a42b3
a2kaido <amane.nikaido+a2kaido@gmail.com>
2019-01-03 20:57:03 +0900

    add history view.

--- app/src/main/java/io/github/a2kaido/barcode/reader/HistoryFragment.kt
+++ app/src/main/java/io/github/a2kaido/barcode/reader/HistoryFragment.kt
@@ -1,10 +1,18 @@
 package io.github.a2kaido.barcode.reader
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import io.github.a2kaido.barcode.reader.domain.model.BarcodeFormat
+import io.github.a2kaido.barcode.reader.domain.model.UrlBarcode
+import kotlinx.android.synthetic.main.fragment_history.*
+import java.util.Date
 
 class HistoryFragment : Fragment() {
 
@@ -13,5 +21,21 @@ class HistoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_history, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = GroupAdapter<ViewHolder>()
+        adapter.add(BarcodeItem(UrlBarcode("https://www.example.com", BarcodeFormat.CODE_128, Date())))
+        adapter.add(BarcodeItem(UrlBarcode("https://www.example.com", BarcodeFormat.CODE_128, Date())))
+        adapter.add(BarcodeItem(UrlBarcode("https://www.example.com", BarcodeFormat.CODE_128, Date())))
+        adapter.add(BarcodeItem(UrlBarcode("https://www.example.com/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", BarcodeFormat.CODE_128, Date())))
+        adapter.add(BarcodeItem(UrlBarcode("https://www.example.com", BarcodeFormat.CODE_128, Date())))
+        adapter.add(BarcodeItem(UrlBarcode("https://www.example.com", BarcodeFormat.CODE_128, Date())))
+        adapter.add(BarcodeItem(UrlBarcode("https://www.example.com", BarcodeFormat.CODE_128, Date())))
+        adapter.add(BarcodeItem(UrlBarcode("https://www.example.com", BarcodeFormat.CODE_128, Date())))
+        history_recycler_view.adapter = adapter
+        history_recycler_view.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
     }
 }
