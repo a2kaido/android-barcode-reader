1e501593a4cba4347076254597b8e10a29912361
a2kaido <amane.nikaido+a2kaido@gmail.com>
2019-01-06 15:30:13 +0900

    add save barcode.

--- app/src/main/java/io/github/a2kaido/barcode/reader/BarcodeApplication.kt
+++ app/src/main/java/io/github/a2kaido/barcode/reader/BarcodeApplication.kt
@@ -1,14 +1,20 @@
 package io.github.a2kaido.barcode.reader
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import io.github.a2kaido.barcode.reader.di.myModule
 import org.koin.android.ext.android.startKoin
 
+@Suppress("unused")
 class BarcodeApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         startKoin(this, listOf(myModule))
+
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+        }
     }
 }
